9213273922c1a86913ddc70767ad605bd7d04bb5
Đỗ Quang Khải <88335848+QuangKhai-dev@users.noreply.github.com>
2025-03-02 20:22:16 +0700

    setup project base

---
+++ README.md
@@ -0,0 +1,17 @@
+# Các thư viện được cài đặt trong dự án
+
+# UI
+
+Thư viện tailwind css, antd, sass và thư viện lucide icon
+
+# Global state và routing
+
+Thư viện react router dom và thư viện redux toolkit
+
+# Thư viện quản lí form và validation
+
+Thư viện Formik và yup
+
+# Thư viện xử API
+
+Thư viện Axios và thư viện React query
---
+++ src/api/axiosConfig.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+export const http = axios.create({
+  baseURL: "https://fiverrnew.cybersoft.edu.vn/api",
+  timeout: 30000,
+  headers: {
+    tokenCybersoft:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA4MiIsIkhldEhhblN0cmluZyI6IjMwLzEwLzIwMjUiLCJIZXRIYW5UaW1lIjoiMTc2MTc4MjQwMDAwMCIsIm5iZiI6MTczNDI4MjAwMCwiZXhwIjoxNzYxOTMwMDAwfQ.CA6BQDcIhQBMqMflDSrrl03owd3zWszFKIb_0AXaF0U",
+  },
+});
